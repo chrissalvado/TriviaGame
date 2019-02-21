279521e6624539b0d0a3bc5c5898583e52fad8fe
chris <chrissalvado@hotmail.com>
2019-02-21 14:42:08 -0600

    started the questions var

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+var Questions = [
+    {
+        prompt: "When the Rolling Stones appeared on The Ed Sullivan Show in 1967, which song were they forced to change the lyrics to so that it wouldn't be censored by the CBS Network censors?\n(a) under my thumb\n\(b) Honkey Tonk Woamn\n\(c)Satisfaction\n\(d) Lets spend the night together",
+        answer: "b"
+        },
+        {
+        prompt: "This guitarist, originally named Johnny, was born in 1942 in Seattle, Washington. He would go on to be arguably the greatest guitarist of all time, though he only lived to be twenty-seven. If I told you what band he was in, it would be way too obvious.\n(a) Jimmy Hendrix \n\(b) Angus Young\n\(c) Jimmy Paige\(d) Chuck Berry",
+        answer: "a"
+        },
+        {
+        prompt: "This Beatle was born in 1942, had a successful career after the band broke up, and played bass.\n(a) John Lennon\n\(b) Ringo Starr\n\(c) George Harrison\n\(d) Paul McCartney",
+        answer: "d"
+         },
+
+        
+    
+
+]
+var score = 0
