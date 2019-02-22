ead9b03162d8bb81e05d78f76c85bebe25f70192
chris <chrissalvado@hotmail.com>
2019-02-22 12:14:28 -0600

    made some file changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,72 @@
+
+var pos = 0, test, test_status, question, guess, guesses, cha, chb, chc, chd, correct = 0;
 var Questions = [
-    {
-        prompt: "When the Rolling Stones appeared on The Ed Sullivan Show in 1967, which song were they forced to change the lyrics to so that it wouldn't be censored by the CBS Network censors?\n(a) under my thumb\n\(b) Honkey Tonk Woamn\n\(c)Satisfaction\n\(d) Lets spend the night together",
-        answer: "b"
-        },
-        {
-        prompt: "This guitarist, originally named Johnny, was born in 1942 in Seattle, Washington. He would go on to be arguably the greatest guitarist of all time, though he only lived to be twenty-seven. If I told you what band he was in, it would be way too obvious.\n(a) Jimmy Hendrix \n\(b) Angus Young\n\(c) Jimmy Paige\(d) Chuck Berry",
-        answer: "a"
-        },
-        {
-        prompt: "This Beatle was born in 1942, had a successful career after the band broke up, and played bass.\n(a) John Lennon\n\(b) Ringo Starr\n\(c) George Harrison\n\(d) Paul McCartney",
-        answer: "d"
-         },
-         {
-        prompt: "This guitarist and co-founder of Van Halen was ranked at seventh on Rolling Stone's best guitar solos list for Erupytion.\n(a) Stevie ray vaughn\n\(b) eddie van halen\n\(c) sammy hagar\n\(d) Slash",
-        answer: "b"
-         },
-         {
-        prompt: "Certainly Billy Joel had many albums that deserved to be considered as one of the greatest of all time. Which of the following is not a Billy Joel album?\n(a) The stranger\n\(b) 52nd street\n\(c) Too Low for Zero\n\(d) Glass Houses",
-        answer: "c"
-         },
-         {
-         prompt: "Billy Joel was once married to what celebrity?\n(a) Brooke Shields\n\(b) Christie Brinkley\n\(c) Stephaine Seymore\n\(d) Ginger Damiels",
-         answer: "b"
-        },
-        {
-        prompt: "This Glam Rocker started his career with The Velvet Underground and went on to have a Walk on the Wild Side solo career. Who is this?/n(a) Lou Reed\n\(b) Adam Ant\n\(c) Phil Colins\n\(d) Frank Zappa",
-        answer: "a"
-        },
-        {
-        prompt: "Which band was named for a high school coach?\n(a) Molly Hathcet\n\(b) Marshall Tucker Band\n\(c) Lynard Skinner\n\(d) Tom Petty",
-        answer: "c"
-        },
-        {
-        prompt: "If I leave here tomorrow...(complete this song line)\n(a) Would you still remember me?\n\(b) Would you be leaving here with me?\n\(c) Will you still be in Tennessee?\n\(d) Would your will include me?",
-        answer: "d"
-        },
-        {
-        prompt:"Bette Davis Eyes topped the Billboard Hot 100 chart in 1981. Who sang it?\n(a) Kim Carnes\n\(b) Blondie\n\(c) Sheena Easton\n\(d) Whitney Houston",
-        answer: "a"
-        },
-        {
-        prompt:"which 80s rock glam band was Brett Micahels the lead singer of?\n(a) Motley Crue\n\(b) Scorpians\n\(c) Whitesnake\n\(d) Poison",
-        answer: "d"
-        },
-]
-var score = 0
 
-$(".Questions").attr(Questions)
-   
-for(var i = 0; i < Questions.length;i++){
-    var response = window.prompt(Questions[i].prompt)
+    ["When the Rolling Stones appeared on The Ed Sullivan Show in 1967, which song were they forced to change the lyrics to so that it wouldn't be censored by the CBS Network censors?", "under my thumb", "Honkey Tonk Woamn", "Satisfaction", "Lets spend the night together", "b" ],
+    
+    ["This guitarist, originally named Johnny, was born in 1942 in Seattle, Washington. He would go on to be arguably the greatest guitarist of all time, though he only lived to be twenty-seven. If I told you what band he was in, it would be way too obvious.", "Jimmy Hendrix", "Angus Young", "Jimmy Paige", "Chuck Berry", "a"],
+    
+    ["This Beatle was born in 1942, had a successful career after the band broke up, and played bass.", "John Lennon", "Ringo Starr", "George Harrison", "Paul McCartney", "d" ],
+    
+    ["This guitarist and co-founder of Van Halen was ranked at seventh on Rolling Stone's best guitar solos list for Erupytion.", "Stevie ray vaughn", "eddie van halen", "sammy hagar", "Slash", "b" ],
 
-    if(response === Questions[i].answer){
-        score++;
-        alert("that is correct sir!")
-    }
-    else{
-        alert("Nope")
-    }
+    ["Certainly Billy Joel had many albums that deserved to be considered as one of the greatest of all time. Which of the following is not a Billy Joel album?", "The stranger", "52nd street", "Too Low for Zero", "Glass Houses", "c" ],
+    
+    ["Billy Joel was once married to what celebrity?", "Brooke Shields", "Christie Brinkley", "Stephaine Seymore", "Ginger Damiels", "b" ],
+     
+    ["This Glam Rocker started his career with The Velvet Underground and went on to have a Walk on the Wild Side solo career. Who is this?", "Lou Reed", "Adam Ant", "Phil Colins", "Frank Zappa", "a" ],
+
+    ["Which band was named for a high school coach?", "Molly Hathcet", "Marshall Tucker Band", "Lynard Skinner", "Tom Petty", "c"],
+    
+    ["If I leave here tomorrow...(complete this song line)", "Would you still remember me?", "Would you be leaving here with me?", "Will you still be in Tennessee?", "Would your will include me?", "d" ],
+    
+    ["Bette Davis Eyes topped the Billboard Hot 100 chart in 1981. Who sang it?", "Kim Carnes", "Blondie", "Sheena Easton", "Whitney Houston", "a"],
+
+    ["which 80s rock glam band was Brett Micahels the lead singer of?", "Motley Crue", "Scorpians", "Whitesnake", "Poison", "d" ],
+];
+
+
+function _(x) {
+    return document.getElementById(x);
 }
+function getQuestions() {
+    test = _("test");
+    console.log(test,"this should be our html test")
+    if(pos >= Questions.length){
+        console.log("checking if statemnets")
+        test.innerHTML = <h2>"You got "+correct" of "+Questions.length+" questions correct"</h2>;
+        _("test_status").innerHTML = "Test is Finished";
+        pos = 0;
+        correct = 0;
+        return false;
+    }
+    _("test_status").innerHTML = "Questions"+(pos+1)+" of "+Questions.length;
+    question = Questions[pos][0];
+    cha = Questions[pos][1];
+    chb = Questions[pos][2];
+    chc = Questions[pos][3];
+    chd = Questions[pos][4];
+    test.innerHTML ="<h3>"+question+"</h3>";
+    test.innerHTML += "<input type='radio' name='guesses' value='a'> "+cha+"><br>";
+    test.innerHTML += "<input type='radio' name='guesses' value='b'> "+chb+"><br>";
+    test.innerHTML += "<input type='radio' name='guesses' value='c'> "+chc+"<br>";
+    test.innerHTML += "<input type='radio' name='guesses' value='d'> "+chd+"<br><br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+    }
+function checkAnswer() {
+    guesses = document.getElementsByName("guesses");
+    for(var i = 0;i <guesses.length;i++){
+        if (guesses[i].checked){
+            guess = guesses[i].value
+        }
+    }
+    if(guess == Questions[pos][5]){
+        correct++
+    }
+    else{alert("that is not correct dummy")}
+    pos++;
+    getQuestions();
+}
+window.addEventListener("load", getQuestions, false);
 
 
